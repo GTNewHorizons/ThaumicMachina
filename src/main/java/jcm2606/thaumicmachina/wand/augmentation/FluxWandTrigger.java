1dbc7af556d40117005151a970f2f7b01ae29cc1
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-05-18 17:41:18 +0200

    remove system.out logging

--- src/main/java/jcm2606/thaumicmachina/wand/augmentation/FluxWandTrigger.java
+++ src/main/java/jcm2606/thaumicmachina/wand/augmentation/FluxWandTrigger.java
@@ -12,7 +12,6 @@ public class FluxWandTrigger implements IWandTriggerManager {
     @Override
     public boolean performTrigger(World world, ItemStack stack, EntityPlayer player, int x, int y, int z, int arg6,
         int arg7) {
-        System.out.println();
         return true;
     }
 }
